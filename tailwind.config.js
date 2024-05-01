cbd42a32389ad3475dca1d26488960057313092b
m-sadiq4641 <sadiq4641@gmail.com>
2024-05-01 15:11:23 +0500

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        bgImg: "url(./images/Body.png)",
+      },
+      colors: {
+        lightorange: "#7f52392d",
+        orangish: "#FCB681",
+        DarkGray: "#0F0F0F",
+      },
+    },
+  },
+  plugins: [],
+};
